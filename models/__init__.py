df039383cb5111fb5e17ac76ca2dac7d547c2449
Roberto Requejo Fernández <roberto.requejo.fdz@gmail.com>
2025-08-09 14:38:47 -0600

    .

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import product_attribute
-from . import product_attribute_value
-from . import product_product
-from . import mrp_production
-from . import sale_order
-
--- wizard/__init__.py
+++
@@ -1,2 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import sale_product_configuration
